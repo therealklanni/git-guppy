d7e3dba36f25ecbe9d3f5b0dfd72424416fc2d01
Kevin Lanni <therealklanni@gmail.com>
2014-08-02 20:55:56 -0700

    add guppy to gulpfile.js
    - add jshint to pre-commit stream
    - initialize guppy tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var verb = require('gulp-verb');
+var guppy = require('./lib/guppy');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 gulp.task('verb', function () {
-  gulp.src(['.verbrc.md'])
+  gulp.src('.verbrc.md')
     .pipe(verb({ dest: 'README.md' }))
     .pipe(gulp.dest('./'));
 });
+
+guppy.stream('pre-commit')
+  .pipe(jshint)
+  .pipe(jshint.reporter, stylish);
+
+guppy.init(gulp);

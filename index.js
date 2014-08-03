0f9d8386fdc597517af575052be3ae77274453ca
Kevin Lanni <therealklanni@gmail.com>
2014-08-02 20:14:01 -0700

    add basic functionality
    nothing more than a proof-of-concept at this point

---
+++ index.js
@@ -0,0 +1,10 @@
+/*!
+ * git-guppy <https://github.com/therealklanni/git-guppy>
+ *
+ * Copyright (c) 2014 Kevin Lanni, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = require('./lib/guppy');

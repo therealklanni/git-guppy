a16a59e809d4f901a86972d4c3bff0fc843aa12e
Kevin Lanni <therealklanni@gmail.com>
2014-08-08 01:16:53 -0700

    add postinstall script

---
+++ scripts/install.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+require('shelljs/global');
+var realpath = require('fs').realpathSync;
+var gitRoot = exec('git rev-parse --show-toplevel').output.slice(0, -1);
+var hooksDir = gitRoot + '/.git/hooks/';
+var gitHooks = require('../lib/git-hooks');
+var code = 0;
+
+gitHooks.forEach(function (hook) {
+  var hookDest = hooksDir + hook;
+
+  if (test('-f', hookDest)) {
+    mv(hookDest, hookDest + '.old');
+  }
+
+  cp(realpath('./scripts/hookfile'), hookDest);
+
+  if (test('-f', hookDest)) {
+    echo(hook + ' installed successfully');
+  } else {
+    code = 1;
+  }
+});
+
+exit(code);

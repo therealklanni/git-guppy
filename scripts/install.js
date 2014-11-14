1eb82ad4a30b5a46c3018368cd91f5b276084c57
Marcello de Sales <Marcello_DeSales@intuit.com>
2014-11-14 08:35:48 -0800

    Issue #7: Modular architecture implementation.
    This commit implements the initial version of a modularized version of
    git-guppy. Instead of using a static list of hooks to be installed, this
    version will use hook collector library to retrieve all the installed
    git-guppy-HOOK_MODULE that's installed by the user. That is:
    
    1. Users will have this version installed first, and it will be empty.
    - No hooks are installed by default.
    
    2. Users will have to install each hook group individually.
    - They will be installed and trigger the installation again.
    
    3. If users install any of the hooks before, the user will be warned
    - Since there's no way to install the hook, we just tell the user to do that.
    
    *	new file:   lib/hook-collector.js
    - Collects the installed hooks from the user by verifying the installed
      packages in the package.json.
    
    *	modified:   scripts/install.js
    - Uses the collected scripts to install.
    
    *	deleted:    lib/git-hooks.json
    - The hooks are computed by the hook-collector.js module.

--- scripts/install.js
+++ scripts/install.js
@@ -4,9 +4,20 @@ require('shelljs/global');
 var realpath = __dirname;
 var gitRoot = exec('git rev-parse --show-toplevel').output.slice(0, -1);
 var hooksDir = gitRoot + '/.git/hooks/';
-var gitHooks = require('../lib/git-hooks');
+var gitHooks = require('./../lib/hook-collector');
+
 var code = 0;
 
+// If that's the first time installing, just 
+if (gitHooks.length == 0) {
+  console.log("Thanks for installing git-guppy! Install any of the hooks available now!");
+  console.log("npm install --save git-guppy-pre-commit git-guppy-pre-commit git-guppy-post-commit ...");
+  console.log("Go to https://github.com/therealklanni/git-guppy for a complete list!");
+  exit(code);
+  // TODO: Provide a full list here to the user with pointers to the git docs http://git-scm.com/docs/githooks
+}
+
+// At least one is installed, so let's install it then
 gitHooks.forEach(function (hook) {
   var hookDest = hooksDir + hook;
 
